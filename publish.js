414d76c70f15424e3b794bfd6782a71d89538d60
18hdm <ceshi1957@gmail.com>
2025-11-26 09:58:14 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'gswmrxtj.xyz',
+	'sonflrig.xyz',
 ];
                                                                                                                   
 var JumpPage="https://18hdm.net";
