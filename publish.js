a5cd7aef237cb72b4a076a8ee59a87045b70f567
18hdm <ceshi1957@gmail.com>
2026-03-04 11:18:38 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'gsdzevop.cc',
+	'upamdtk.cc',
 ];
                                                                                                                   
 var JumpPage="https://18hdm.net";
