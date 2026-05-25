a0094fa61b413ee0225f27a851933682d0c37696
18hdm <ceshi1957@gmail.com>
2026-05-25 10:26:18 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'ncsgkmty.cc',
+	'lqxfaigm.cc',
 ];
                                                                                                                   
 var JumpPage="https://18hdm.net";
