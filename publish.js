c92a76be67739db6290f100db822c0f8e01e376b
18hdm <ceshi1957@gmail.com>
2026-05-29 15:36:55 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'cztzatdk.com',
+	'yrvyhoyf.cc',
 ];
                                                                                                                   
 var JumpPage="https://18hdm.net";
