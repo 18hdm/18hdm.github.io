7e04c714d051be5969aa62f1b8780eb2d7e52d9d
18hdm <ceshi1957@gmail.com>
2026-06-03 09:10:16 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'fbinilopt.cc',
+	'oassssuu.cc',
 ];
                                                                                                                   
 var JumpPage="https://18hdm.net";
