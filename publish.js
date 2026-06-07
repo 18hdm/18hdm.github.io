e9810f4e2f9d3e1f5bdfb65f62d77db42fe450e8
18hdm <ceshi1957@gmail.com>
2026-06-08 06:37:30 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'spowbprt.cc',
+	'lmshfveuf.cc',
 ];
                                                                                                                   
 var JumpPage="https://18hdm.net";
