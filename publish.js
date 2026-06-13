191ea8ab0a76bb0350a9a149b7e13d6737a2ab4c
18hdm <ceshi1957@gmail.com>
2026-06-13 17:37:57 +0700

    Update publish.js

--- publish.js
+++ publish.js
@@ -37,7 +37,7 @@ var emails = [
 ];
 
 var urls=[
-	'ezvavujig.cc',
+	'evmjcdivp.cc',
 ];
                                                                                                                   
 var JumpPage="https://18hdm.net";
